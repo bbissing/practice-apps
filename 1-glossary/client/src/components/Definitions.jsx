6b59ef351d6f4b16a2fa989327a32affe958ba3b
bbissing <bbissing101@gmail.com>
2022-09-16 19:45:38 -0500

    Bare Minimum Requirements - Practice Apps - Glossary

--- 1-glossary/client/src/components/Definitions.jsx
+++ 1-glossary/client/src/components/Definitions.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Definition from './Definition.jsx';
-import DefinitionClass from './DefinitionClass.jsx';
 
 function Definitions(props) {
   console.log('in Definitions.jsx - searchedTerm', props.searched);
@@ -10,13 +9,13 @@ function Definitions(props) {
       console.log('in Definitions.jsx - searchedTerm - in map - searchedTerm', props.searched);
       if (word.word === props.searched) {
         return (
-          <DefinitionClass key={word._id} word={word} update={props.update} delete={props.delete}/>
+          <Definition key={word._id} word={word} update={props.update} delete={props.delete}/>
         )
       }
     })
   } else return props.wordList.data.map(word => {
     return (
-      <DefinitionClass key={word._id} word={word} update={props.update} delete={props.delete}/>
+      <Definition key={word._id} word={word} update={props.update} delete={props.delete}/>
     )
   })
 }
--- 1-glossary/client/src/components/SearchBar.jsx
+++ 1-glossary/client/src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ class SearchBar extends React.Component{
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({value: event.target.value}, () => { if(this.state.value === '') { this.props.search(null); } });
   }
 
   handleSubmit(event) {
